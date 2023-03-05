b74222b16e8850d3997805844fa3016b4c7a24e7
Maryellen Marino <maryellendmarino@gmail.com>
2023-03-04 23:11:21 -0500

    prices page modifications

--- lib/prices.dart
+++ lib/prices.dart
@@ -19,18 +19,59 @@ class _Prices extends State<Prices> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             const SizedBox(height: 100),
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Text(
-                'Inflator Raider',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontFamily: 'Helvetica',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+            Row(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 0, left: 110),
+                  child: Text(
+                    'Inflator Raider',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontFamily: 'Helvetica',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30),
+                    child: SizedBox(
+                        width: 35,
+                        height: 35,
+
+                        child: Stack(
+                            children: <Widget>[
+                              Positioned(
+                                  top: 0,
+                                  left: 0,
+                                  child: Container(
+                                      width: 35,
+                                      height: 35,
+                                      decoration: const BoxDecoration(
+                                        color : Color.fromRGBO(76, 192, 129, 1),
+                                        borderRadius : BorderRadius.all(Radius.elliptical(35, 35)),
+                                      )
+                                  )
+                              ),Positioned(
+                                  top: 5,
+                                  left: 4,
+                                  child: Container(
+                                      width: 25,
+                                      height: 25,
+                                      decoration: const BoxDecoration(
+                                        image : DecorationImage(
+                                            image: AssetImage('assets/SettingsIcon.png'),
+                                            fit: BoxFit.fitWidth
+                                        ),
+                                      )
+                                  )
+                              ),
+                            ]
+                        )
+                    )
+                )
+              ]
             ),
             Column(
               children: [
